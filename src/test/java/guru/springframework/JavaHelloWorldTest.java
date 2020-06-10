e0033804fd254485552863062963173324458103
Soraya Jimenez <soraya.jimenez.castillo@akersolutions.com>
2020-06-10 10:39:54 +0200

    Junit4 tests

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    public void getHello() {
+        JavaHelloWorld jhw = new JavaHelloWorld();
+        assertEquals("Hello World", jhw.getHello());
+    }
+}
